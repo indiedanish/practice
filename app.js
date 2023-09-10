97f8b37eeff439251857970e24ab8ec088051b29
indiedanish <danishbutt_db@hotmail.com>
2023-09-10 15:11:48 +0500

    update app and added jenkins file

---
+++ app.js
@@ -0,0 +1,21 @@
+// Import the required modules
+const http = require('http');
+
+// Define the hostname and port
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  // Set the response status and headers
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  // Send the "Hello, World!" message as the response
+  res.end('Hello, World!\n');
+});
+
+// Start the server and listen on the specified hostname and port
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
